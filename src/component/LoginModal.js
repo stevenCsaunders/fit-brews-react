b837bf24612fcb5aec5805ee1f4bd6468b085543
Alex Kim <312akim@gmail.com>
2020-05-11 16:46:32 -0500

    Login Form & NavLink Fix

--- src/component/LoginModal.js
+++ src/component/LoginModal.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import {Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody,
+    Label, Row, Col } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
-
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 
 class LoginModal extends Component {
     constructor(props) {
@@ -10,6 +13,10 @@ class LoginModal extends Component {
         this.state = {
             isModalOpen: false
         };
+    }
+
+    handleSubmit = (values) => {
+        alert("Current state is: " + JSON.stringify(values));
     }
 
     render () {
@@ -20,14 +27,75 @@ class LoginModal extends Component {
         };
 
         return (
-            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                <ModalHeader toggle={this.toggleModal}>
-                    Login
-                </ModalHeader>
-                <ModalBody>
-                    
-                </ModalBody>
-            </Modal>
+            <>
+                <Row>
+                    <Col>
+                        <Button onClick={toggleModal}>
+                            Login
+                        </Button>
+                    </Col>
+                </Row>
+                <Modal isOpen={this.state.isModalOpen} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>
+                        Login
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.submitComment(values)}>
+                            <Row class="form-group">
+                                <Col>
+                                    <Label htmlFor="email">
+                                        Your Email Address
+                                    </Label>
+                                    <Control.text model=".email" name="email" id="email" className="form-control" placeholder="Email Address" 
+                                    validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(2),
+                                    }}
+                                    />
+                                    <Errors className="text-danger"
+                                    model=".email"
+                                    show="touched"
+                                    components="div"
+                                    messages={{
+                                        required: "Required",
+                                        minLength: "Must be at least 2 characters",
+                                        maxLength: "Must be 15 characters or less",
+                                    }}/>
+                                </Col>
+                            </Row>
+                            <Row class="form-group">
+                                <Col>
+                                    <Label htmlFor="password">
+                                        Your Password
+                                    </Label>
+                                    <Control.text model=".password" name="password" id="password" className="form-control" placeholder="Password" 
+                                    validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(2),
+                                    }}
+                                    />
+                                    <Errors className="text-danger"
+                                    model=".password"
+                                    show="touched"
+                                    components="div"
+                                    messages={{
+                                        required: "Required",
+                                        minLength: "Must be at least 2 characters",
+                                        maxLength: "Must be 15 characters or less",
+                                    }}/>
+                                </Col>
+                            </Row>
+                            <Button type="submit" color="primary">
+                                    Login
+                            </Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </>
         );
     }
 }
+
+export default LoginModal;
